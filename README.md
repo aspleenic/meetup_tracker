ccb9e20a45f3b515664baabbdb21f83a6aceb25a
PJ Hagerty <pjhagerty@gmail.com>
2013-08-21 10:49:47 -0400

    Removing rails - going for Sinatra

--- README.md
+++
@@ -1,6 +0,0 @@
-meetup_tracker
-==============
-
-Meetups WorldWide Tracker
-
-The goal is to be able to track any type of meetup that is being run through meetups.com and display that info.
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Meetups::Application.config.secret_token = '25ae15004174f4b4f3a454127c12440160308685aaf0894621a37a475594ca65de13413cf975554406b0c31bdf85eaa5c2fcbf8414a7560bfda6e53da7c7d597'
