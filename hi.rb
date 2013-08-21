bf33c80200d678484efecd3215b6edc29ab5a15e
PJ Hagerty <pjhagerty@gmail.com>
2013-08-21 15:15:55 -0400

    Adding an index page and breakouts on the different meetups

--- hi.rb
+++ hi.rb
@@ -6,21 +6,24 @@ require 'rmeetup'
 require 'rest_client'
 require 'addressable/uri'
 
-# get '/' do
-# 	RMeetup::Client.api_key = "682d733452163d471f4656620674a53"
-#   results = RMeetup::Client.fetch(:events,{:zip => "14226", })
-#   results.each do |result|
-#     print "#{result.name}"
-#   end
-# end
-
 get '/' do
-  api_result = RestClient.get (Addressable::URI.parse('http://api.meetup.com/groups.json/?zip=14226&topic=ruby&order=members&key=682d733452163d471f4656620674a53').normalize.to_str)
-  api_result = JSON.parse(api_result)
-  if api_result.empty?
-    "You failed"
-  else
-    api_result
-  end
-  # erb :meetup_table, :locals => {api_result: api_result}
+  erb :index
+end
+
+get '/ruby' do
+  api_result = RestClient.get (Addressable::URI.parse('http://api.meetup.com/groups.json/?topic=ruby&order=members&key=682d733452163d471f4656620674a53').normalize.to_str)
+  result = JSON.parse(api_result) 
+  erb :meetup_table, :locals => {result: result} 
+end
+
+get '/php' do
+  api_result = RestClient.get (Addressable::URI.parse('http://api.meetup.com/groups.json/?topic=php&order=members&key=682d733452163d471f4656620674a53').normalize.to_str)
+  result = JSON.parse(api_result) 
+  erb :meetup_table, :locals => {result: result}
+end
+
+get '/nodejs' do
+  api_result = RestClient.get (Addressable::URI.parse('http://api.meetup.com/groups.json/?topic=nodejs&order=members&key=682d733452163d471f4656620674a53').normalize.to_str)
+  result = JSON.parse(api_result) 
+  erb :meetup_table, :locals => {result: result}
 end
