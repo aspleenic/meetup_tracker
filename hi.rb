5c6d0fc98f4b11d4680aa5bf18084e89112380df
PJ Hagerty <pjhagerty@gmail.com>
2013-08-22 14:10:48 -0400

    Getting through the hash before we start ti iterate over the hash.

--- hi.rb
+++ hi.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'bundler/setup'
 require 'sinatra'
 require 'open-uri'
 require 'json'
@@ -11,19 +12,22 @@ get '/' do
 end
 
 get '/ruby' do
-  api_result = RestClient.get (Addressable::URI.parse('http://api.meetup.com/groups.json/?topic=ruby&order=members&key=682d733452163d471f4656620674a53').normalize.to_str)
-  result = JSON.parse(api_result) 
-  erb :meetup_table, :locals => {result: result} 
+  api_result = RestClient.get 'http://api.meetup.com/groups.json/?&topic=ruby&order=members&key=682d733452163d471f4656620674a53'
+  jhash = JSON.parse(api_result)
+  results = jhash['results'][0]['name']
+  erb :meetup_table, :locals => {result: results}
 end
 
 get '/php' do
-  api_result = RestClient.get (Addressable::URI.parse('http://api.meetup.com/groups.json/?topic=php&order=members&key=682d733452163d471f4656620674a53').normalize.to_str)
-  result = JSON.parse(api_result) 
-  erb :meetup_table, :locals => {result: result}
+  api_result = RestClient.get 'http://api.meetup.com/groups.json/?&topic=php&order=members&key=682d733452163d471f4656620674a53'
+  jhash = JSON.parse(api_result)
+  results = jhash['results'][0]['name']
+  erb :meetup_table, :locals => {result: results}
 end
 
 get '/nodejs' do
-  api_result = RestClient.get (Addressable::URI.parse('http://api.meetup.com/groups.json/?topic=nodejs&order=members&key=682d733452163d471f4656620674a53').normalize.to_str)
-  result = JSON.parse(api_result) 
-  erb :meetup_table, :locals => {result: result}
+  api_result = RestClient.get 'http://api.meetup.com/groups.json/?&topic=nodejs&order=members&key=682d733452163d471f4656620674a53'
+  jhash = JSON.parse(api_result)
+  results = jhash['results'][0]['name']
+  erb :meetup_table, :locals => {result: results}
 end
