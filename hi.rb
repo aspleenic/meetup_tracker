c44f2ec5e90fa18d1a243cc609f446a26cd21ef1
PJ Hagerty <pjhagerty@gmail.com>
2013-09-03 15:41:34 -0400

    Adding devops and cloud searches as well

--- hi.rb
+++ hi.rb
@@ -74,3 +74,43 @@ get '/nodejs' do
   erb :meetup_table, :locals => {result: output, counter: counter}
 end
 
+get '/cloud' do
+  api_result = RestClient.get 'http://api.meetup.com/groups.json/?&topic=cloud&order=members&key=682d733452163d471f4656620674a53'
+  jhash = JSON.parse(api_result)
+  counter = jhash['results'].count
+  output = ''
+
+  jhash['results'].each do |j|
+    name = j['name']
+    city = j['city']
+    focus = j['who']
+    count = j['members']
+    contact = j['organizer_name']
+    link = j['link']
+    country = j['country']
+    
+    output << "<tr><td>#{name}</td> <td><a href = '#{link}' target = _new>#{city}</a></td><td>#{country}</td><td>#{focus}</td> <td>#{count}</td><td>#{contact}</td></tr>"
+  end
+  erb :meetup_table, :locals => {result: output, counter: counter}
+end
+
+get '/devops' do
+  api_result = RestClient.get 'http://api.meetup.com/groups.json/?&topic=devops&order=members&key=682d733452163d471f4656620674a53'
+  jhash = JSON.parse(api_result)
+  counter = jhash['results'].count
+  output = ''
+
+  jhash['results'].each do |j|
+    name = j['name']
+    city = j['city']
+    focus = j['who']
+    count = j['members']
+    contact = j['organizer_name']
+    link = j['link']
+    country = j['country']
+    
+    output << "<tr><td>#{name}</td> <td><a href = '#{link}' target = _new>#{city}</a></td><td>#{country}</td><td>#{focus}</td> <td>#{count}</td><td>#{contact}</td></tr>"
+  end
+  erb :meetup_table, :locals => {result: output, counter: counter}
+end
+
