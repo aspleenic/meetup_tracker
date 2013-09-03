2230ea0986de95ac1434c9402cc89effbff25664
PJ Hagerty <pjhagerty@gmail.com>
2013-09-03 15:15:26 -0400

    Matching changes for PHP and node

--- hi.rb
+++ hi.rb
@@ -12,6 +12,8 @@ get '/' do
   erb :index
 end
 
+# TODO: Add devops, Cloud, CTO
+
 get '/ruby' do
   api_result = RestClient.get 'http://api.meetup.com/groups.json/?&topic=ruby&order=members&key=682d733452163d471f4656620674a53'
   jhash = JSON.parse(api_result)
@@ -45,8 +47,9 @@ get '/php' do
     count = j['members']
     contact = j['organizer_name']
     link = j['link']
+    country = j['country']
     
-    output << "<tr><td>#{name}</td> <td>#{city}</td> <td>#{focus}</td> <td>#{count}</td><td>#{contact}</td><td><a href = '#{link}' target = _new>Link</a></td></tr>"
+    output << "<tr><td>#{name}</td> <td><a href = '#{link}' target = _new>#{city}</a></td><td>#{country}</td><td>#{focus}</td> <td>#{count}</td><td>#{contact}</td></tr>"
   end
   erb :meetup_table, :locals => {result: output, counter: counter}
 end
@@ -64,8 +67,9 @@ get '/nodejs' do
     count = j['members']
     contact = j['organizer_name']
     link = j['link']
+    country = j['country']
     
-    output << "<tr><td>#{name}</td> <td>#{city}</td> <td>#{focus}</td> <td>#{count}</td><td>#{contact}</td><td><a href = '#{link}' target = _new>Link</a></td></tr>"
+    output << "<tr><td>#{name}</td> <td><a href = '#{link}' target = _new>#{city}</a></td><td>#{country}</td><td>#{focus}</td> <td>#{count}</td><td>#{contact}</td></tr>"
   end
   erb :meetup_table, :locals => {result: output, counter: counter}
 end
