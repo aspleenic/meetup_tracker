75194c369634a2f1a3678fea1966e124249c6526
PJ Hagerty <pjhagerty@gmail.com>
2013-09-06 08:55:52 -0400

    Added CTO meetups

--- hi.rb
+++ hi.rb
@@ -12,7 +12,7 @@ get '/' do
   erb :index
 end
 
-# TODO: Add CTO
+# TODO: DRY this up!!
 
 get '/ruby' do
   api_result = RestClient.get 'http://api.meetup.com/groups.json/?&topic=ruby&order=members&key=682d733452163d471f4656620674a53'
@@ -114,3 +114,22 @@ get '/devops' do
   erb :meetup_table, :locals => {result: output, counter: counter}
 end
 
+get '/CTO' do
+  api_result = RestClient.get 'http://api.meetup.com/groups.json/?&topic=CTO&order=members&key=682d733452163d471f4656620674a53'
+  jhash = JSON.parse(api_result)
+  counter = jhash['results'].count
+  output = ''
+
+  jhash['results'].each do |j|
+    name = j['name']
+    city = j['city']
+    focus = j['who']
+    count = j['members']
+    contact = j['organizer_name']
+    link = j['link']
+    country = j['country']
+    
+    output << "<tr><td>#{name}</td> <td><a href = '#{link}' target = _new>#{city}</a></td><td>#{country.upcase}</td><td>#{focus}</td> <td>#{count}</td><td>#{contact}</td></tr>"
+  end
+  erb :meetup_table, :locals => {result: output, counter: counter}
+end
